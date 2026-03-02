8652b49bd6168515ce63b646a0f67fa491c5d781
TrestoXD <vitogamer08@gmail.com>
2026-03-02 14:53:09 +0100

    0.1.0
    POPUP AÑADIDO Y MEJORAS EN MOVIL

---
+++ Codigos/WIP.js
@@ -0,0 +1,28 @@
+
+//VARIABLES
+
+const _debug = false;
+
+const _popup = document.getElementById('popup');
+const _popupbackground = document.getElementById('popupbackground');
+
+let _popupdata = sessionStorage.getItem("wippopup");
+
+// CODIGO DEL POPUP
+
+if(_popupdata == null || _debug == true){
+    console.log("SACANDO POPUP!");
+
+}else{
+    _popup.style.display = 'none';
+    _popupbackground.style.display = 'none';
+
+    console.log("POPUP YA VISTO");
+}
+
+_popup.addEventListener('click', () => {
+    _popup.style.display = 'none';
+    _popupbackground.style.display = 'none';
+
+    sessionStorage.setItem("wippopup", "true");
+});
