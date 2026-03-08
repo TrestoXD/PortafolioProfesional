1ccd4b32d377a8758d6abf41c12fe1b4ed99c7d3
TrestoXD <vitogamer08@gmail.com>
2026-03-08 22:54:53 +0100

    4.0.0

---
+++ Codigos/mar.js
@@ -0,0 +1,7 @@
+//VARIABLES
+
+const _marquee = document.querySelector(".mar-text")
+
+//CODIGO DEL Marquee
+
+_marquee.innerHTML = "¡Bienvenido! a mi portafolio electronico, Este es un espacio donde comparto mis proyectos, exploralos cuanto quieras a cualquier duda o interés, puedes contactame. ¡Sigue siempre adelante!"
