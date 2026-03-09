927074b7af1e452fd20110258eb51a6d8e3310c4
TrestoXD <vitogamer08@gmail.com>
2026-03-09 17:15:30 +0100

    4.1.1
    IDIOMAS COMPLETAMENTE AÑADIDOS AL SOBREMI

--- Codigos/idiomas.js
+++ Codigos/idiomas.js
@@ -9,7 +9,7 @@
 // 1 = h1 / p / marquee
 // 2 = h2 / a / botón
 // 3 = h3 / b
-// 4 = h4
+// 4 = h4 / spam
 
 // TERCER NUMERO
 // Hueco disponible empezando por 1
@@ -54,6 +54,9 @@ function cambiartexto(entry){
         document.getElementById("023").innerText = "SOFT SKILLS:";
         document.getElementById("024").innerText = "ESTUDIOS:";
 
+        document.getElementById("041").innerText = "NACIMIENTO:";
+        document.getElementById("042").innerText = "ESTADOS:"
+
         //TEXTO 
         document.getElementById("111").innerText = `Mi nombre es Vito Minaya González, fotógrafo aficionado apasionado de capturar momentos extraordinarios que suceden en mi vida cotidiana. Mientras que la fotografía me encanta, mi trabajo a tiempo completo es en la informática y la ciencia. Creyendo que entender las bases técnicas lleva a hacer las cosas mejor, lo que me motiva a crear experiencias que no solo se vean atractivas, sino que estén meticulosamente construidas para el rendimiento y la accesibilidad.`
         document.getElementById("112").innerText = "CFGM en Sistemas microinformáticos y redes.";
@@ -67,6 +70,20 @@ function cambiartexto(entry){
         document.getElementById("127").innerText = "Iniciativa y autonomía";
         document.getElementById("128").innerText = "Instituto de educación secundaria la Garrotxa.";
         document.getElementById("129").innerText = "Centro de estudios Monlau"
+
+        document.getElementById("131").innerText = "FECHA: ";
+        document.getElementById("132").innerText = "LUGAR: ";
+        document.getElementById("133").innerText = "SEXO: ";
+        document.getElementById("134").innerText = "NACIONALIDAD: ";
+        document.getElementById("135").innerText = "ESTADO ACADEMICO: ";
+        document.getElementById("136").innerText = "OCUPACIÓN: ";
+
+        document.getElementById("141").innerText = "18 DE ABRIL DE 2008";
+        document.getElementById("142").innerText = "BARCELONA";
+        document.getElementById("143").innerText = "HOMBRE";
+        document.getElementById("144").innerText = "ESPAÑOL";
+        document.getElementById("145").innerText = "ESTUDIANTE";
+        document.getElementById("146").innerText = "TRABAJANDO";
 
         //OTROS
         document.getElementById("211").innerText = "¡Bienvenido! a mi portafolio electrónico. Este es un espacio donde comparto mis proyectos; explóralos cuanto quieras. Ante cualquier duda o interés, puedes contactarme. ¡Sigue siempre adelante!";
@@ -90,6 +107,9 @@ function cambiartexto(entry){
         document.getElementById("023").innerText = "SOFT SKILLS:";
         document.getElementById("024").innerText = "ESTUDIS:";
 
+        document.getElementById("041").innerText = "NAIXEMENT:";
+        document.getElementById("042").innerText = "ESTATS:"
+
         //TEXTO 
         document.getElementById("111").innerText = `El meu nom és Vito Minaya González, fotògraf aficionat apassionat de capturar moments extraordinaris que succeeixen en la meva vida quotidiana. Mentre que la fotografia m'encanta, el meu treball a temps complet és en la informàtica i la ciència. Creient que entendre les bases tècniques porta a fer les coses millor, la qual cosa em motiva a crear experiències que no sols es vegin atractives, sinó que estiguin meticulosament construïdes per al rendiment i l'accessibilitat.`
         document.getElementById("112").innerText = "CFGM en Sistemes microinformàtics i xarxes.";
@@ -103,6 +123,20 @@ function cambiartexto(entry){
         document.getElementById("127").innerText = "Iniciativa i autonomia";
         document.getElementById("128").innerText = "IES la Garrotxa";
         document.getElementById("129").innerText = "Centre d'estudis Monlau"
+
+        document.getElementById("131").innerText = "DATA: ";
+        document.getElementById("132").innerText = "LLOC: ";
+        document.getElementById("133").innerText = "SEXE: ";
+        document.getElementById("134").innerText = "NACIONALITAT: ";
+        document.getElementById("135").innerText = "ESTAT ACADÈMIC: ";
+        document.getElementById("136").innerText = "OCUPACIÓ: ";
+
+        document.getElementById("141").innerText = "18 D'ABRIL DE 2008";
+        document.getElementById("142").innerText = "BARCELONA";
+        document.getElementById("143").innerText = "HOME";
+        document.getElementById("144").innerText = "ESPAÑOL";
+        document.getElementById("145").innerText = "ESTUDIANT";
+        document.getElementById("146").innerText = "TREBALLANT";
     
         //OTROS
         document.getElementById("211").innerText = "Benvingut! al meu portafolis electrònic. Aquest és un espai on comparteixo els meus projectes; explora'ls quant vulguis. Davant qualsevol dubte o interès, pots contactar-me. Segueix sempre avanci!";
@@ -126,6 +160,9 @@ function cambiartexto(entry){
         document.getElementById("023").innerText = "SOFT SKILLS:";
         document.getElementById("024").innerText = "STUDIES:";
 
+        document.getElementById("041").innerText = "BIRTH:";
+        document.getElementById("042").innerText = "STATES:"
+
         //TEXTO 
         document.getElementById("111").innerText = `My name is Vito Minaya González, an amateur photographer passionate to capture extraordinary moments that happen in my daily life. While I love photography, my full-time work is in computer science and science. Beliving that undestanding the thecnical bases leads to doing thigs better, which motivates me to create experiences that are not only attractive, but that are meticulously built for performance and accessibility`
         document.getElementById("112").innerText = "CFGM in Microcomputer systems & Networks.";
@@ -139,6 +176,20 @@ function cambiartexto(entry){
         document.getElementById("127").innerText = "Initative and Autonomy";
         document.getElementById("128").innerText = "La Garrotxa Secondary Education Institution";
         document.getElementById("129").innerHTML = "Study Center Monlau"
+
+        document.getElementById("131").innerText = "DATE: ";
+        document.getElementById("132").innerText = "PLACE: ";
+        document.getElementById("133").innerText = "GENDER: ";
+        document.getElementById("134").innerText = "NATIONALITY: ";
+        document.getElementById("135").innerText = "ACADEMIC STATE: ";
+        document.getElementById("136").innerText = "OCCUPATION: ";
+
+        document.getElementById("141").innerText = "APRIL 18TH OF 2008";
+        document.getElementById("142").innerText = "BARCELONA";
+        document.getElementById("143").innerText = "MAN";
+        document.getElementById("144").innerText = "SPANISH";
+        document.getElementById("145").innerText = "STUDENT";
+        document.getElementById("146").innerText = "WORKING";
         
         //OTROS
         document.getElementById("211").innerText = "Welcome to my electronic portfolio. This is a space where I share my projects; explore them as you want. Before any doubt or interest, you can contact me. Keep moving Forward!";
